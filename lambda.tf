56ae0ac0c536903dd4f2f8215add4241d2c6eab0
Pubudu Jayawardana <pubudusj@gmail.com>
2020-06-13 00:04:21 +0200

    Lambda function added

---
+++ lambda.tf
@@ -0,0 +1,21 @@
+data "template_file" "lambda_assume_role_policy" {
+  template = file("./policies/lambda_assume_role.json")
+}
+
+resource "aws_iam_role" "lambda-assume-role-policy" {
+  assume_role_policy = data.template_file.lambda_assume_role_policy.rendered
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-policy-role-attachment" {
+  policy_arn =  "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role = aws_iam_role.lambda-assume-role-policy.name
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "demo-alb-target-function"
+  filename = "lambda.zip"
+  handler = "index.handler"
+  role = aws_iam_role.lambda-assume-role-policy.arn
+  runtime = "nodejs12.x"
+  source_code_hash = filebase64sha256("lambda.zip")
+}
